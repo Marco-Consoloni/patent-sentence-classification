599f0d88bce188ccc18fd3f90b9a7542f063afec
Federico Galatolo <galatolo.federico@gmail.com>
2025-01-12 15:21:25 +0000

    added gitignore file

--- src/model.py
+++ src/model.py
@@ -1,16 +1,14 @@
 import torch
 import pytorch_lightning as pl
-#from transformers import AdamW
 
 from src.config import load_config
 
 class PatentClassifier(pl.LightningModule):
-    def __init__(self, model, tokenizer, learning_rate=2e-5):
+    def __init__(self, model, tokenizer):
         super().__init__()
         self.cfg = load_config()
         self.model = model
         self.tokenizer = tokenizer
-        #self.learning_rate = learning_rate
 
     def forward(self, **inputs):
         return self.model(**inputs)
@@ -39,7 +37,3 @@ class PatentClassifier(pl.LightningModule):
     def configure_optimizers(self):
         optimizer = getattr(torch.optim, self.cfg.optimizer.name)
         return optimizer(self.parameters(), **self.cfg.optimizer.args)
-
-    #def configure_optimizers(self):
-    #  optimizer = AdamW(self.parameters(), lr=self.learning_rate)
-    #  return optimizer
